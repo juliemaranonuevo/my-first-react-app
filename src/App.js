dccd274a9462016ddb152868c550ed2ddfffb561
Juliemar Anonuevo <mr.juliemar.anonuevo@gmail.com>
2022-11-14 22:35:29 +0800

    Adding Links & Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
+/**********************************************
+ * PAGES
+ **********************************************/
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 
+/**********************************************
+ * COMPONENTS/LAYOUTS
+ **********************************************/
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-metup" element={<NewMeetupPage />} />
