ac0d6b8d2bc649f20e0192dfd9765704c1244eab
Juliemar Anonuevo <mr.juliemar.anonuevo@gmail.com>
2022-11-16 21:45:07 +0800

    Outputting Lists Of Data & Components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,66 @@
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image: 'https://i.picsum.photos/id/991/200/200.jpg?hmac=zdrg88cOolAGaFqNdqLZG2ECaXB0AjpYxU8PNyq3dBw',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description: 'This is a first'
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image: 'https://i.picsum.photos/id/827/200/300.jpg?hmac=0Q7y5JGXuxSXgO7VUvdNhXC4yoAupOJiKmRS9RoPqs8',
+    address: 'Meetupstreet 5, 12345 Meetup City 2',
+    description: 'This is a second'
+  },
+  {
+    id: 'm3',
+    title: 'This is a third meetup',
+    image: 'https://i.picsum.photos/id/827/200/300.jpg?hmac=0Q7y5JGXuxSXgO7VUvdNhXC4yoAupOJiKmRS9RoPqs8',
+    address: 'Meetupstreet 5, 12345 Meetup City 2',
+    description: 'This is a second'
+  }
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul>
+
+
+      {/* <table style={{ border: '1px solid', width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={{ border: '1px solid' }}>Id</th>
+            <th style={{ border: '1px solid' }}>Title</th>
+            <th style={{ border: '1px solid' }}>Image</th>
+            <th style={{ border: '1px solid' }}>Address</th>
+            <th style={{ border: '1px solid' }}>Descriptiom</th>
+          </tr>
+        </thead>
+        <tbody>
+          {DUMMY_DATA.map((meetup) => {
+            return (
+              <tr key={meetup.id}>
+                <td style={{ border: '1px solid' }}>{meetup.id}</td>
+                <td style={{ border: '1px solid' }}>{meetup.title}</td>
+                <td style={{ border: '1px solid' }}>
+                  <img src={meetup.image}></img>
+                </td>
+                <td style={{ border: '1px solid' }}>{meetup.address}</td>
+                <td style={{ border: '1px solid' }}>{meetup.description}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table> */}
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
