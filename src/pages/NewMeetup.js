af581e92b776c31d1083bb6464432f4e202afdce
Juliemar Anonuevo <mr.juliemar.anonuevo@gmail.com>
2022-12-05 17:57:56 +0800

    Sending a POST Http Request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,13 +3,25 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
 
   const addMeetUpHandler = (meetUpData) => {
-    console.log(meetUpData);
+    /***************************************************************************************
+     *  FETCH IS A DEFAULT BUILT IN JAVASCRIPT FUNCTION THAT ALLOWS US SEND HTTP REQUESTS 
+     ***************************************************************************************/
+    fetch(
+      'https://react-getting-started-1edb8-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetUpData),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
+    );
   }
 
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetUp={addMeetUpHandler}/>
+      <NewMeetupForm onAddMeetUp={addMeetUpHandler} />
     </section>
   );
 }
